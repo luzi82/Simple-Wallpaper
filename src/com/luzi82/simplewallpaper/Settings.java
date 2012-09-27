3e71eeaf9864361dfbeaf346f652490583a49317
luzi82 <luzi82@gmail.com>
2012-09-27 19:54:04 +0800

    fix a bug where cropped size not equal to screen size

--- src/com/luzi82/simplewallpaper/Settings.java
+++ src/com/luzi82/simplewallpaper/Settings.java
@@ -1,6 +1,9 @@
 package com.luzi82.simplewallpaper;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
@@ -8,6 +11,10 @@ import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.PackageManager.NameNotFoundException;
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.CompressFormat;
+import android.graphics.BitmapFactory;
+import android.graphics.Rect;
 import android.net.Uri;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -84,40 +91,15 @@ public class Settings extends PreferenceActivity {
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 		if (resultCode != RESULT_OK)
 			return;
-		Log.v(LiveWallpaper.LOG_TAG, "onActivityResult " + requestCode);
+		// Log.v(LiveWallpaper.LOG_TAG, "onActivityResult " + requestCode);
 		if ((requestCode == ACTIVITY_LANDSCAPE_CHOOSE) || (requestCode == ACTIVITY_PORTRAIT_CHOOSE)) {
-			// DisplayMetrics dm=new DisplayMetrics();
-			Display display = getWindowManager().getDefaultDisplay();
-			// display.getMetrics(dm);
-			//
-			// int ww = dm.widthPixels;
-			// int hh = dm.heightPixels;
+			Rect rect = getSize(requestCode == ACTIVITY_LANDSCAPE_CHOOSE);
+			int w = rect.width();
+			int h = rect.height();
 
-			// TODO remove hardcode
-			int ww = display.getWidth();
-			int hh = display.getHeight();
-
-			try {
-				Method getRawWidth = Display.class.getMethod("getRawWidth");
-				Method getRawHeight = Display.class.getMethod("getRawHeight");
-				ww = (Integer) getRawWidth.invoke(display);
-				hh = (Integer) getRawHeight.invoke(display);
-			} catch (SecurityException e) {
-			} catch (NoSuchMethodException e) {
-			} catch (IllegalArgumentException e) {
-			} catch (IllegalAccessException e) {
-			} catch (InvocationTargetException e) {
-			}
-
-			int l = (ww > hh) ? ww : hh;
-			int s = (ww > hh) ? hh : ww;
-
-			int w = (requestCode == ACTIVITY_LANDSCAPE_CHOOSE) ? l : s;
-			int h = (requestCode == ACTIVITY_LANDSCAPE_CHOOSE) ? s : l;
-
-			Log.v(LiveWallpaper.LOG_TAG, "w " + w + " h " + h);
+			// Log.v(LiveWallpaper.LOG_TAG, "w " + w + " h " + h);
 			Uri uri = Uri.fromFile(tmpFile());
-			Log.v(LiveWallpaper.LOG_TAG, uri.getPath());
+			// Log.v(LiveWallpaper.LOG_TAG, uri.getPath());
 
 			Intent intent = new Intent("com.android.camera.action.CROP");
 			intent.setType("image/*");
@@ -134,13 +116,38 @@ public class Settings extends PreferenceActivity {
 			startActivityForResult(Intent.createChooser(intent, getString(R.string.activity_crop)), (requestCode == ACTIVITY_LANDSCAPE_CHOOSE) ? ACTIVITY_LANDSCAPE_CROP : ACTIVITY_PORTRAIT_CROP);
 		} else if ((requestCode == ACTIVITY_LANDSCAPE_CROP) || (requestCode == ACTIVITY_PORTRAIT_CROP)) {
 			// Log.v(LiveWallpaper.LOG_TAG, data.getData().getPath());
+			try {
+				boolean landscape = (requestCode == ACTIVITY_LANDSCAPE_CROP);
+				String outPath = (landscape ? LiveWallpaper.landscapeFile() : LiveWallpaper.portraitFile()).getAbsolutePath();
 
-			File f = tmpFile();
-			if (f.exists()) {
-				File out = (requestCode == ACTIVITY_LANDSCAPE_CROP) ? LiveWallpaper.landscapeFile() : LiveWallpaper.portraitFile(); // out.delete();
-				out.delete();
-				out = (requestCode == ACTIVITY_LANDSCAPE_CROP) ? LiveWallpaper.landscapeFile() : LiveWallpaper.portraitFile();
-				f.renameTo(out);
+				File f = tmpFile();
+				if (f.exists()) {
+					Rect rect = getSize(landscape);
+					int w = rect.width();
+					int h = rect.height();
+
+					Bitmap bitmap = BitmapFactory.decodeFile(f.getAbsolutePath());
+					// Log.v(LiveWallpaper.LOG_TAG, "w " + bitmap.getWidth() +
+					// " h " + bitmap.getHeight());
+					if (bitmap.getWidth() == w && bitmap.getHeight() == h) {
+						File out = new File(outPath);
+						out.delete();
+						out = new File(outPath);
+						f.renameTo(out);
+					} else {
+						Bitmap b2 = Bitmap.createScaledBitmap(bitmap, w, h, true);
+						File out = new File(outPath);
+						out.delete();
+						FileOutputStream fos = new FileOutputStream(outPath);
+						b2.compress(CompressFormat.PNG, 0, fos);
+						fos.flush();
+						fos.close();
+					}
+				}
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
 
 			// try {
@@ -162,6 +169,33 @@ public class Settings extends PreferenceActivity {
 			// e.printStackTrace();
 			// }
 		}
+	}
+
+	Rect getSize(boolean aLandscape) {
+		Display display = getWindowManager().getDefaultDisplay();
+
+		int ww = display.getWidth();
+		int hh = display.getHeight();
+
+		try {
+			Method getRawWidth = Display.class.getMethod("getRawWidth");
+			Method getRawHeight = Display.class.getMethod("getRawHeight");
+			ww = (Integer) getRawWidth.invoke(display);
+			hh = (Integer) getRawHeight.invoke(display);
+		} catch (SecurityException e) {
+		} catch (NoSuchMethodException e) {
+		} catch (IllegalArgumentException e) {
+		} catch (IllegalAccessException e) {
+		} catch (InvocationTargetException e) {
+		}
+
+		int l = (ww > hh) ? ww : hh;
+		int s = (ww > hh) ? hh : ww;
+
+		int w = aLandscape ? l : s;
+		int h = aLandscape ? s : l;
+
+		return new Rect(0, 0, w, h);
 	}
 
 	static File tmpFile() {
